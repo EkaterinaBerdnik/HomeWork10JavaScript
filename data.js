84b048f02aadbffde65c0ebe4fa065f5afae2c85
Ekaterina <ek.berdnik@yandex.ru>
2023-03-10 13:15:50 +0300

    дз к семинару 10

---
+++ data.js
@@ -0,0 +1,146 @@
+const dataInfo = `[
+    {
+        "id": 102693,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 102851,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/rcam/RRB_486265291EDR_F0481570RHAZ00323M_.JPG",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424905,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631300503690E01_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424906,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631300305227E03_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424907,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631290503689E01_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424908,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631290305226E03_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424909,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631280503688E0B_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424910,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631280305225E03_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424911,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631270503687E03_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424912,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631270305224E03_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424913,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631260503686E03_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424914,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631260305223E03_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424915,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631250503685E01_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424916,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631250305222E03_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424917,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631240503684E03_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424918,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631240305221E03_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424919,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631230503683E01_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424920,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631230305220E02_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424921,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631220503682E01_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424922,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631220305219E01_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424923,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631210503681E01_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424924,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631210305218E01_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424925,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631200503680E0C_DXXX.jpg",
+        "date": "2015-05-30"
+      },
+      {
+        "id": 424926,
+        "sol": 1000,
+        "img": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631200305217E01_DXXX.jpg",
+        "date": "2015-05-30"
+      }
+]`
